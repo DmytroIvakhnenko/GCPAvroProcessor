e399805c63b4e9cee8f0a4855f6306c200987ecc
DmytroIvakhnenko <dimsaid2dim@gmail.com>
2020-10-13 20:05:35 +0300

    initial commit

---
+++ src/main/java/io/github/dmytroivakhnenko/gcpavroprocessor/GcpAvroProcessorApplication.java
@@ -0,0 +1,13 @@
+package io.github.dmytroivakhnenko.gcpavroprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GcpAvroProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcpAvroProcessorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/dmytroivakhnenko/gcpavroprocessor/GcpAvroProcessorApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.dmytroivakhnenko.gcpavroprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcpAvroProcessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
