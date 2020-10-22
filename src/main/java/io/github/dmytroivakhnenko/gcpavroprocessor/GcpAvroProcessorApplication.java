8928f7288d80d179b4509e81c529278e8af2fa4c
DmytroIvakhnenko <dimsaid2dim@gmail.com>
2020-10-22 12:26:20 +0300

    Exclude Spring Integration configuration

--- src/main/java/io/github/dmytroivakhnenko/gcpavroprocessor/GcpAvroProcessorApplication.java
+++ src/main/java/io/github/dmytroivakhnenko/gcpavroprocessor/GcpAvroProcessorApplication.java
@@ -2,12 +2,13 @@ package io.github.dmytroivakhnenko.gcpavroprocessor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.integration.IntegrationAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = IntegrationAutoConfiguration.class)
 public class GcpAvroProcessorApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(GcpAvroProcessorApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(GcpAvroProcessorApplication.class, args);
+    }
 
 }
